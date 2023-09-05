a2cce8092fcc0b8f2ea7abb1f6de8f3641b1f6d4
Oliver <115661717+OliKirk@users.noreply.github.com>
2023-09-05 20:22:45 +0200

    Updated backend til vores frontend

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,10 @@ import fs from "fs/promises";
 import cors from "cors";
 
 const app = express();
+const port = 3000;
+
 app.use(express.json());
 app.use(cors());
-const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server started on www.https://localhost:${port}`);
@@ -15,7 +16,7 @@ app.get("/artists", async (req, res) => {
   const data = await fs.readFile("backend/data.json");
   const artists = JSON.parse(data);
 
-  res.send(artists);
+  res.json(artists);
 });
 
 app.get("/artists/:id", async (req, res) => {
@@ -27,6 +28,8 @@ app.get("/artists/:id", async (req, res) => {
 
   let artist = artists.find((artist) => artist.id === id);
 
+  fs.writeFile("backend/data.json", JSON.stringify(artists));
+
   if (!artist) {
     res.status(404).json({ error: "Artist not found" });
   } else {
@@ -34,23 +37,49 @@ app.get("/artists/:id", async (req, res) => {
   }
 });
 
-app.post("/artists", (req, res) => {
-  console.log(req.body);
-  const newArtist = {
-    id: new Date().getTime(),
-    task: req.body.task,
-    completed: req.body.completed,
-  };
-  console.log(newArtist);
+app.post("/artists", async (req, res) => {
+  const newArtist = req.body;
+  newArtist.id = new Date().getTime();
+
+  const data = await fs.readFile("backend/data.json");
+  const artists = JSON.parse(data);
 
   artists.push(newArtist);
+  fs.writeFile("backend/data.json", JSON.stringify(artists));
   res.json(artists);
 });
 
-app.put("/artists/:id", (req, res) => {
-  res.json(req.parameter);
+app.put("/artists/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+
+  const data = await fs.readFile("backend/data.json");
+  const artists = JSON.parse(data);
+  let artistToUpdate = artists.find((artist) => artist.id == id);
+  console.log(artistToUpdate);
+  const body = req.body;
+
+  console.log(body);
+  artistToUpdate.name = body.name;
+  artistToUpdate.birthday = body.birthday;
+  artistToUpdate.genres = body.genre;
+  artistToUpdate.labels = body.labels;
+  artistToUpdate.website = body.website;
+  artistToUpdate.image = body.image;
+  artistToUpdate.shortDescription = body.shortDescription;
+
+  fs.writeFile("backend/data.json", JSON.stringify(artists));
+  res.json(artists);
 });
 
-app.delete("/artists/:id", (req, res) => {
-  res.send("Got a DELETE request");
+app.delete("/artists/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const data = await fs.readFile("data.json");
+  const artists = JSON.parse(data);
+
+  const newArtists = artists.filter((artist) => artist.id !== id);
+  fs.writeFile("backend/data.json", JSON.stringify(newArtists));
+
+  res.json(artists);
 });
